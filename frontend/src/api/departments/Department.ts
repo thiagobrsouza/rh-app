88134f8491339a71ec09903ad829a4fb5b8ad6bd
thiagobrsouza <thiago.glespaul@gmail.com>
2022-07-12 08:45:32 -0300

    Using axios to show department list

---
+++ frontend/src/api/departments/Department.ts
@@ -0,0 +1,5 @@
+export interface Department {
+  id?: any;
+  name?: string;
+  createdAt?: string;
+}
--- frontend/src/components/departments/List.tsx
+++ frontend/src/components/departments/List.tsx
@@ -1,11 +1,23 @@
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { useEffect, useState } from 'react';
+import { Department } from '../../api/departments/Department';
+import http from '../../api/http';
 import { ButtonDialog } from "../common/ButtonDialog";
 import { Dialog } from "../common/Dialog";
 import { Card } from "../layout/Card";
 import { Layout } from "../layout/Layout";
 
 export const DepartmentList = () => {
+
+  const [departments, setDepartments] = useState<Department[]>([]);
+
+  useEffect(() => {
+    http.get('/departments').then(response => {
+      setDepartments(response.data);
+    })
+  }, []);
+
   return (
     <Layout>
       <Card title="Department List">
@@ -13,9 +25,9 @@ export const DepartmentList = () => {
         <Dialog title="Add or Update a Department" id="formDepartment">
 
         </Dialog>
-        <DataTable>
-          <Column header="Code" />
-          <Column header="Name" />
+        <DataTable value={departments} rowHover>
+          <Column header="Code" field='id' />
+          <Column header="Name" field='name' />
         </DataTable>
       </Card>
     </Layout>
